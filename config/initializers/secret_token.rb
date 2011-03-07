989487008cd846be12f9c91185f3abcf805c2315
lexer <lexer.box@gmail.com>
2011-03-08 01:13:58 +0500

    add default rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railsnew::Application.config.secret_token = 'e864f8948bd2cecfcea2758d9f13bebd3526dc65dc249b898549081fd28ae2dd1270cc674be513d2f5a85d852cfab5e5081a9537f05a4aef9c8521973c1169df'
