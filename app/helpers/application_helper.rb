c633cf15b405c209246b5d3fa19875649a8ab70e
hazzik <hazzik@gmail.com>
2011-03-20 01:37:42 +0500

    js_init helper which writes javascript view initialization into page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
       Dir[config.javascripts_dir + File::SEPARATOR + path].map { |s| s.sub(config.assets_dir, "") }
     end
   end
+
+  def js_init
+    Haml::Engine.new(":javascript
+  $(function(){
+     App.views.#{controller_name}.#{action_name.titleize}.init();
+  });
+").render
+  end
 end
